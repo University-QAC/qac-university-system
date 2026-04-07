654036afe57d50335dd8337a22a0bc69be3dd0fe
Mostafa El-Nahas <mostafanahas777@gmail.com>
2026-04-08 01:11:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QAC University System
---
+++ src/main/java/com/effortcure/qac/QualityAccreditationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.effortcure.qac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityAccreditationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityAccreditationSystemApplication.class, args);
+	}
+
+}
